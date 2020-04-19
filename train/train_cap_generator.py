beb147dd3eb7177eb66d232cca32a94cfa96a69b
MarcusNerva <hadrianus_1@163.com>
2020-04-20 07:56:08 +0800

    modify hypothesis for semantics training

--- train/train_cap_generator.py
+++ train/train_cap_generator.py
@@ -189,6 +189,7 @@ def train(opt):
 
         if opt.self_critical_after != -1 and epoch >= opt.self_critical_after:
             sc_flag = True
+            opt.save_checkpoint_every = 500
         else:
             sc_flag = False
         # sc_flag = True
