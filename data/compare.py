c18d188d0ed8a6aad12f638f5006845db596ba0e
MarcusNerva <hadrianus_1@163.com>
2020-05-05 10:16:01 +0800

    analyse data

--- data/compare.py
+++ data/compare.py
@@ -27,7 +27,7 @@ def test(opt, infermodel, embed):
         sentences.append(content1[key])
     embeddings = infermodel.encode(sentences, bsize=128, tokenize=True)
     for i in range(len(embeddings)):
-        temp = infermodel.encode([sentences[i]], bsize=128, tokenize=True)
+        temp = infermodel.encode([sentences[i]], bsize=128, tokenize=True)[0]
         if not math.fabs(1 - cosine(embeddings[i], temp)) < EPS:
             print(store[i])
             print(sentences[i])
