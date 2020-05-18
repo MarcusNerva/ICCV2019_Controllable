91c8d706d50f7c40d19af6c879cc3bc07124a2c3
MarcusNerva <hadrianus_1@163.com>
2020-05-18 09:12:08 +0800

    modify extract_prediction.py

--- eval/extract_prediction.py
+++ eval/extract_prediction.py
@@ -67,5 +67,5 @@ if __name__ == '__main__':
     with open(prediction_path, 'wb') as f:
         pickle.dump(total_dict, f)
 
-    with open(os.path.join(opt.data_path, 'extracted_test_prediction.pkl'), 'wb') as f:
-        pickle.dump(test_prediction, f)
+    # with open(os.path.join(opt.data_path, 'extracted_test_prediction.pkl'), 'wb') as f:
+    #     pickle.dump(test_prediction, f)
